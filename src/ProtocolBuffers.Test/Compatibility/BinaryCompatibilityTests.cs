085008a5b4144ed3240f4237935e4a3f8e25dbd6
rogerk <rogerk@localhost>
2011-10-03 16:00:22 -0500

    Adding silverlight testing framework and statlight test runner

---
+++ src/ProtocolBuffers.Test/Compatibility/BinaryCompatibilityTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Google.ProtocolBuffers.Compatibility
+{
+    [TestFixture]
+    public class BinaryCompatibilityTests : CompatibilityTests
+    {
+        protected override object SerializeMessage<TMessage, TBuilder>(TMessage message)
+        {
+            byte[] bresult = message.ToByteArray();
+            return bresult;
+        }
+
+        protected override TBuilder DeserializeMessage<TMessage, TBuilder>(object message, TBuilder builder, ExtensionRegistry registry)
+        {
+            return builder.MergeFrom((byte[])message, registry);
+        }
+    }
+}
